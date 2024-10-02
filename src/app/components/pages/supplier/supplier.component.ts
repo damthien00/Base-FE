cfe5cef9a16729c34e087efe5c50c9848d8f848f
anhdungIT <shikikan.master2k2@gmail.com>
2024-10-02 14:13:10 +0700

    --

--- src/app/components/pages/supplier/supplier.component.ts
+++ src/app/components/pages/supplier/supplier.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-supplier',
-  standalone: true,
-  imports: [],
   templateUrl: './supplier.component.html',
   styleUrl: './supplier.component.scss'
 })
